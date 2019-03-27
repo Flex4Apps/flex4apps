551ed7f6c2a11fb22c34bae633a5b300b2bc3692
Erik Vanherck <evanherc@Eriks-MacBook-Pro.local>
2019-03-27 14:46:15 +0100

    Remove the local documentation build as readthedocs is the best solution and done automatically

--- build.sh
+++
@@ -1,8 +0,0 @@
-docker run --name buildTheDocs --rm \
-      -e "Project=Flex4apps" \
-      -e "Author=Till Witt, Johannes Berg, Alex Nowak" \
-      -e "Version=v0.1" \
-      -v "$(pwd)/compose:/project/compose" \
-      -v "$(pwd)/docs:/project/input" \
-      -v "$(pwd)/output:/project/output" \
-      -i -t sphinxneeds_img
